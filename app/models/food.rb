fb96c0911788f37b56c4d579b66bf6f254ffed3a
Ridwan Hady <ridwanhady31@gmail.com>
2020-03-14 22:13:31 +0700

    Add food model spec invalid if description is empty

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,3 +1,4 @@
 class Food < ApplicationRecord
   validates_presence_of :name
+  validates_presence_of :description
 end
--- spec/models/food_spec.rb
+++ spec/models/food_spec.rb
@@ -14,6 +14,10 @@ RSpec.describe Food, type: :model do
       it "should fail when name is empty" do
         expect(Food.new(@params.slice(:description, :price))).to_not be_valid
       end
+
+      it "should fail when description is empty" do
+        expect(Food.new(@params.slice(:name, :price))).to_not be_valid
+      end
     end
   end
 end
