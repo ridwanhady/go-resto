7bd2cb3d23662f981c6506092385e6b70aaaa146
Ridwan Hady <ridwanhady31@gmail.com>
2020-03-14 22:43:30 +0700

    Add food model spec invalid if description length is more than 100

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,5 +1,5 @@
 class Food < ApplicationRecord
   validates_presence_of :name
-  validates_presence_of :description
+  validates :description, presence: true, length: { maximum: 100 }
   validates :price, presence: true, numericality: { only_float: true }
 end
--- spec/models/food_spec.rb
+++ spec/models/food_spec.rb
@@ -26,7 +26,15 @@ RSpec.describe Food, type: :model do
       it "should fail when the price is not float" do
         expect(Food.create({ name: "Meatballs", description: "A round shaped meat", price: "asd" })).to_not be_valid
       end
-      
+
+      it "should fail when the description length is more than 100" do
+        expect(Food.create({ 
+          name: "Meatballs", 
+          description: "scoSQYjFebApttSjenovV5R8PN3OcTt14GvTZQX9JIDUe62cQsmpuqvymqgfaxvZQGeZI7d9Wes7EbSmFkYPFs4hm0ZowcpgxhYwP5A2oJmHYN", 
+          price: 100
+          })
+        ).to_not be_valid
+      end      
     end
   end
 end
